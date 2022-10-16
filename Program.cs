b29263621ccc0027f177b350607801f3b88a0a43
alexey_marusin <lexxx351@mail.ru>
2022-10-16 09:59:26 +0500

    improved the program

--- Program.cs
+++ Program.cs
@@ -3,8 +3,29 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пjльзоваться коллекциями, лучше обойтись исключительно массивами.
 
-string[] array = {"main", "url", "8", "more", "change"};
-for(int i = 0; i < array.GetLength(0); i++)
+Console.WriteLine("Из введенного массива строк сформируем новый массив из строк, длина которых меньше либо равна 3 символа.");
+Console.Write("Какое количество элементов будет в массиве? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+for(int i = 0; i < n; i++)
 {
-    if (array[i].Length <= 3) Console.Write(array[i] + ", ");
+    Console.Write($"Введите элемент: ");
+    string m = Convert.ToString(Console.ReadLine());
+    array[i] = m;
 } 
+
+Console.Write("Новый массив: ");
+int l = 0;
+string[] newArray = new string[n];
+for(int j = 0; j < n; j++)
+{
+    if(array[j].Length <= n)
+    {
+        newArray[l] = array[j];
+        Console.Write(newArray[l] + "  ");
+        l++;
+    }
+}
+
+
